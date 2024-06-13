fe4bb0971414a9776b7ff22df5a021d6809ee708
MariaMeza1997 <125325866+MariaMeza1997@users.noreply.github.com>
2024-06-13 17:21:02 -0600

    header-page

--- header-page.php
+++ header-page.php
@@ -1,29 +1,36 @@
-<section id="top-bar" class="bg-black">
+<section id="top-bar" class="top-bar">
 
     <div class="contain flex center centerV">
 
         <div class="flex centerV">
-            <a href="tel:+527777874088" class="no-mg">¡CONTÁCTAME! +52 777 787 4088</a>
+            <a href="tel:+527777874088">¡CONTÁCTAME! +52 777 787 4088</a>
         </div>
 
     </div>
 
 </section>
 
-<section id="middle-bar" class="contain flex justify centerV">
 
-    <a href="/" class="logo">
-        <img loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo-white.png" alt="logo" width="206" height="81">
-    </a>
+<section id="middle-bar" >
 
-    <nav class="flex centerV">
-        <a href="#">Inicio</a>
-        <a href="#te-cuento">Te cuento más sobre mí </a>
-        <a href="#servicios">Nuestros servicios</a>
-        <a href="">¿Por qué acudir con nosotros?</a>
-        <a href="">¿Cómo cuidar la salud de mi mascota?</a>
-        <a href="">Preguntas frecuentes</a>
-        <a href="">Contacto</a>
-    </nav>
+    <div  class="logo-header flex justify centerV">
+        <a href="/" class="logo">
+            <img loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Logo.png" alt="logo" width="206" height="81">
+        </a>
+    </div>
 
+    <div class="contain flex justify centerV nav">
+        <nav class="flex centerV">
+            <a href="#">Inicio</a>
+            <a href="#te-cuento">Te cuento más sobre mí </a>
+            <a href="#servicios">Nuestros servicios</a>
+            <a href="">¿Por qué acudir con nosotros?</a>
+            <a href="">¿Cómo cuidar la salud de mi mascota?</a>
+            <a href="">Preguntas frecuentes</a>
+            <a href="">Contacto</a>
+        </nav>
+
+    </div>
+
+   
 </section>
