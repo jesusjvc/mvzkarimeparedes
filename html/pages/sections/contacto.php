388ec58879840c995fd4a97bcc6b31f5aa873fd5
MariaMeza1997 <125325866+MariaMeza1997@users.noreply.github.com>
2024-06-12 23:29:27 -0600

    cambios

---
+++ html/pages/sections/contacto.php
@@ -0,0 +1,9 @@
+<section id="contacto" class="contacto">
+
+    <div class="imagen">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Logo3.png" alt="Logo" width="399"
+                        height="157">
+    </div>
+
+</section>
+
