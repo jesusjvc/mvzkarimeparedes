0f57497aeac4aea9be44ed1519e989dcc39f4d34
MariaMeza1997 <125325866+MariaMeza1997@users.noreply.github.com>
2024-06-13 18:23:18 -0600

    Contacto-part1

--- html/pages/sections/contacto.php
+++ html/pages/sections/contacto.php
@@ -1,9 +1,34 @@
-<section id="contacto" class="contacto">
+<section id="contacto" >
 
-    <div class="imagen">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Logo3.png" alt="Logo" width="399"
-                        height="157">
-    </div>
+        <div class="imagen">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Logo3.png" alt="Logo" width="206" height="157">
+           
+        </div>
+
+        <hr>
+
+
+        <div class="information-contacto">
+            <div class="contain">
+                <div class="grid-contacto">
+
+                    <div class="grid-contacto-information">
+                        <h1>Datos de contacto:</h1>
+                        <hr>
+                    </div>
+
+                    <div class="grid-contacto-form">
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+      
+            
+        
 
 </section>
 
