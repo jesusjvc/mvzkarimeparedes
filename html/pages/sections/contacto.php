94425712a8929e31a60f026b518ff579cc6f11ff
MariaMeza1997 <125325866+MariaMeza1997@users.noreply.github.com>
2024-06-14 17:55:31 -0600

    change-contacto

--- html/pages/sections/contacto.php
+++ html/pages/sections/contacto.php
@@ -15,6 +15,50 @@
                     <div class="grid-contacto-information">
                         <h1>Datos de contacto:</h1>
                         <hr>
+
+                        <div>
+
+                            <div class="grid-contacto-information-media-item">
+                                <div class="grid-contacto-information-media-item-imagen">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/ubicacion.png" alt="Logo" width="65" height="65">
+                                </div>
+
+                                <div class="grid-contacto-information-media-item-text">
+                                    <p>Calle 5 de mayo 515, Col. Campo Sotelo, Temixco, Morelos.</p>
+                                </div>
+                            </div>
+
+                            <div class="grid-contacto-information-media-item">
+                                <div class="grid-contacto-information-media-item-imagen">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/whatsapp.png" alt="Logo" width="65" height="65">
+                                </div>
+
+                                <div class="grid-contacto-information-media-item-text">
+                                    <p>+52 777 787 6588</p>
+                                </div>
+                            </div>
+
+                            <div class="grid-contacto-information-media-item">
+                                <div class="grid-contacto-information-media-item-imagen">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/instagram.png" alt="Logo" width="65" height="65">
+                                </div>
+
+                                <div class="grid-contacto-information-media-item-text">
+                                    <p>mvz.karimepc</p>
+                                </div>
+                            </div>
+
+                            <div class="grid-contacto-information-media-item">
+                                <div class="grid-contacto-information-media-item-imagen">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/email.png" alt="Logo" width="65" height="65">
+                                </div>
+
+                                <div class="grid-contacto-information-media-item-text">
+                                    <p>karime@mvzkarimeparedes.mx</p>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
 
                     <div class="grid-contacto-form">
