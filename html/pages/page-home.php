779563c78dd44b051094b556ee394abfc8280898
MariaMeza1997 <125325866+MariaMeza1997@users.noreply.github.com>
2024-06-19 23:07:10 -0600

    change

--- html/pages/page-home.php
+++ html/pages/page-home.php
@@ -1,4 +1,5 @@
 
+<?php add_revslider('slider-1'); ?>
 <?php include_once 'sections/barra-movil.php' ?>
 <?php include_once 'sections/faq.php' ?>
 <?php include_once 'sections/testimonios.php' ?>
--- html/pages/sections/testimonios.php
+++ html/pages/sections/testimonios.php
@@ -1,7 +1,7 @@
 <div id="testimonios" class="padding">
     <div class="contain">
-        <h2>Comentarios de mis Pacientes</h2>
-        <hr>
+        <h2>Nuestros clientes te comparten su opinión.</h2>
+    
         <?php echo do_shortcode("[trustindex no-registration=google]") ?>
     </div>
 </div>
