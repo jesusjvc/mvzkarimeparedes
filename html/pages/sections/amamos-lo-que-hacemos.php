5fb5e77d4aeaddbb9b7da3bc10cbe3136a9964a4
MariaMeza1997 <125325866+MariaMeza1997@users.noreply.github.com>
2024-06-21 15:41:43 -0600

    change

---
+++ html/pages/sections/amamos-lo-que-hacemos.php
@@ -0,0 +1,14 @@
+<section id="amamos" class="padding">
+    <div class="contain">
+
+        <h1>¡Amamos lo que hacemos, por eso amamos a tu mascota!</h1>
+        <p>Tu mascota merece el mejor trato y nosotros lo sabemos. Te muestro parte del proceso para cuidarla.</p>
+
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/video-imagen.png" alt="imagen">
+
+        <!-- <video class="block auto" id="video1" controls poster="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/video-imagen.png">
+            <source src="#" type="video/mp4">
+        </video> -->
+
+    </div>
+</section>
