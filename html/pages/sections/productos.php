aea0787a4a9adf69bdeef63d495ea5c9638e1ca1
MariaMeza1997 <125325866+MariaMeza1997@users.noreply.github.com>
2024-06-24 19:18:44 -0600

    change

--- html/pages/sections/productos.php
+++ html/pages/sections/productos.php
@@ -3,7 +3,10 @@
         
         <h1>Farmacia</h1>
         <p>En mi farmacia podrás encontrar una gran variedad de medicamentos, los cuales sólo debes adquirir y dar a tu mascota bajo supervisión médica. Recuerda nunca automedicar a tu mascota. Aquí te comparto algunos de ellos:</p>
-        <?php woo_product_slider( 75 ); ?>
         
+        <div class="carrousel">
+            <?php echo do_shortcode( ' [gswoo id=115] ' ); ?>
+        </div>
+
     </div>
 </section>
