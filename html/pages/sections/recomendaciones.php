7bccc18a71418b6519a59110c6703f1df620ae3f
MariaMeza1997 <125325866+MariaMeza1997@users.noreply.github.com>
2024-06-25 18:44:13 -0600

    change

--- html/pages/sections/recomendaciones.php
+++ html/pages/sections/recomendaciones.php
@@ -6,6 +6,10 @@
         <p>Recuerda llevarla a chequeos periódicos y seguir siempre las indicaciones del veterinario.</p>
 
         <div class="diagrama-recomendaciones">
+
+            <div class="imagen-movil-perro">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/perro.png" alt="imagen">
+            </div>
             
         
             <div class="div-imagen-diagrama">
@@ -20,52 +24,52 @@
                 
                 </div>
 
-                <div></div>
+                <div class="class-none-movil-recomendaciones"></div>
 
                 <div class="item-vacuna">
                     <div class="item-veterinario-image">
-                        <img class="image-centro-recomendaciones" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/veterinario.png" alt="imagen">
+                        <img class="image-centro-recomendaciones" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/vacunas.png" alt="imagen">
                     </div>
-                    <div class="item-veterinario-texto">Llevarlo al <br> veterinario.</div>
+                    <div class="item-veterinario-texto">Aplicar sus<br> vacunas a tiempo.</div>
                 
                 </div>
 
                 <!-- Line-two -->
                 <div class="item-desparasitar">
-                    <div class="item-veterinario-texto">Llevarlo al <br> veterinario.</div>
+                    <div class="item-veterinario-texto">Desparasitarlo a<br> tiempo.</div>
                     <div class="item-veterinario-image">
-                        <img class="image-centro-recomendaciones" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/veterinario.png" alt="imagen">
+                        <img class="image-centro-recomendaciones" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/desparasitarlo.png" alt="imagen">
                     </div>
                 
                 </div>
 
-                <div></div>
+                <div class="class-none-movil-recomendaciones"></div>
 
                 <div class="item-alimentar">
                     <div class="item-veterinario-image">
-                        <img class="image-centro-recomendaciones" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/veterinario.png" alt="imagen">
+                        <img class="image-centro-recomendaciones" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/alimento.png" alt="imagen">
                     </div>
-                    <div class="item-veterinario-texto">Llevarlo al <br> veterinario.</div>
+                    <div class="item-veterinario-texto">Alimentarlo<br> adecuadamente.</div>
                 
                 </div>
 
                  <!-- Line-three -->
 
                  <div class="item-actividad">
-                    <div class="item-veterinario-texto">Llevarlo al <br> veterinario.</div>
+                    <div class="item-veterinario-texto">Buena actividad<br> física.</div>
                     <div class="item-veterinario-image">
-                        <img class="image-centro-recomendaciones" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/veterinario.png" alt="imagen">
+                        <img class="image-centro-recomendaciones" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/actividad.png" alt="imagen">
                     </div>
                 
                 </div>
 
-                <div></div>
+                <div class="class-none-movil-recomendaciones"></div>
 
                 <div class="item-carino">
                     <div class="item-veterinario-image">
-                        <img class="image-centro-recomendaciones" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/veterinario.png" alt="imagen">
+                        <img class="image-centro-recomendaciones" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/carino.png" alt="imagen">
                     </div>
-                    <div class="item-veterinario-texto">Llevarlo al <br> veterinario.</div>
+                    <div class="item-veterinario-texto">Darle cariño<br> y atención.</div>
                 
                 </div>
 
