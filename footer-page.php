b87281cf43ca44a8131b0015a30f524f50a8acda
MariaMeza1997 <125325866+MariaMeza1997@users.noreply.github.com>
2024-07-22 20:16:07 -0600

    cambios

--- footer-page.php
+++ footer-page.php
@@ -1,5 +1,5 @@
 <section id="ubicacion" class="ubicacion">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d793.3318674690005!2d-99.21051826872753!3d18.942739658612453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cddfe9af86e9c7%3A0xa7c550804b5e94ec!2sGASTROENTER%C3%93LOGO%20-GASTRIKA%C2%AE-%20DR.SIGFRIDO%20GARC%C3%8DA%20ROM%C3%81N!5e0!3m2!1ses!2smx!4v1704850895570!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3776.0816664401973!2d-99.21453692502885!3d18.839036282317394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdd89535150711%3A0x1f7ad3805f7a608b!2s5%20de%20Mayo%20515%2C%20Miguel%20Hidalgo%2C%2062578%20Temixco%2C%20Mor.!5e0!3m2!1sen!2smx!4v1721700247123!5m2!1sen!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 </section>
 
--- html/pages/sections/barra-movil.php
+++ html/pages/sections/barra-movil.php
@@ -4,6 +4,6 @@
     </a>
 
     <a id="cta-whatsapp" target="blank" href="https://api.whatsapp.com/send/?phone=5277778765881&text=Hola%2C+quiero+informacion&type=phone_number&app_absent=0" class="flex center">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/WhatsApp.svg" alt="WhatsApp" width="5" height="6">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/whatsApp.svg" alt="WhatsApp" width="5" height="6">
     </a>
 </nav>
