3d5613dfb9d4fe14ed28614b4198c60184ca4a36
Gracian Mata <gmv10_8@hotmail.com>
2025-06-05 15:28:14 -0600

    add banner

--- html/pages/sections/banner.php
+++ html/pages/sections/banner.php
@@ -1,26 +1,31 @@
 <section id="banner" class="padding">
     <div class="contain">
         <div>
-            <img class="banner" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/fondomvz.jpg" alt="imagen">
-            <img class="banner-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Imagen.png" alt="imagen">
+            <h2>Los acompañantes de cuatro patas necesitan la mejor consideración</h2>
+            <p>Conoce todos los <b>servicios </b> que tenemos para <b>tu mascota.</b> </p>
+            <button>Más información</button>
         </div>
     </div>
 </section>
 <style>
-	#banner .banner{
-    width: 100%;
-    display: block;
-    margin: auto;
-}
-#banner .banner-responsive{
-    display: none;
-}
-@media(max-width:600px){
-    #banner .banner{
-        display: none;
+	#banner{
+        background-image: url(/assets/img/fondomvz.jpg);
+        background-size: cover;
     }
-    #banner .banner-responsive{
-        display: block;
+    #banner h2{
+        color: #000;
+    }
+    #banner p{
+        color: #7b7a7a;
+    }
+    #banner button{
+        background-color: #3eb0c8;
+        border-radius: 1rem;
+    }
+@media(max-width:600px){
+    #banner{
+        background-image:url(/assets/img/Imagen.png) ;
+        background-size: cover;
     }
 }
 </style>
